4b22c23456a9a9b25f8ebdb6e5a4453ca6b677c2
Dara Hak <dara.hak@gmail.com>
2022-12-03 10:02:26 +0100

    Project setup

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    env: {
+        node: true,
+        es2022: true,
+    },
+    extends: ['eslint:recommended'],
+    parserOptions: {
+        sourceType: 'module',
+    },
+}
